a807e817c729f7fad3738c48f02c645ea1577e83
Ayo Omotosho <aomotosho4@gmail.com>
2024-02-25 17:04:58 -0600

    finish letters css

--- script.js
+++ script.js
@@ -1,2 +1,60 @@
-console.log("Hello, World!");
-alert("Hello, World!");
+let alphaNumeric = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+function generateRandomArray(length) {
+  let randomArray = [];
+  for (let i = 0; i < length; i++) {
+    randomArray.push(
+      alphaNumeric[Math.floor(Math.random() * alphaNumeric.length)],
+    );
+  }
+  return randomArray;
+}
+
+const textContainer = document.querySelector(".text-container");
+const randomArray = generateRandomArray(12);
+
+randomArray.map((item) => {
+  const span = document.createElement("span");
+  span.classList.add("letter");
+  span.textContent = item;
+  textContainer.appendChild(span);
+});
+
+console.log(randomArray.join(""));
