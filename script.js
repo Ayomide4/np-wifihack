964fb06ecd4c2cd5a6746e28ce8dd873989274d8
Ayo Omotosho <aomotosho4@gmail.com>
2024-02-29 15:55:00 -0600

    add progress bar, correct/incorrect js

--- script.js
+++ script.js
@@ -50,13 +50,13 @@ checkmarkSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
 checkmarkSvg.setAttribute("width", "20");
 checkmarkSvg.setAttribute("height", "20");
 checkmarkSvg.setAttribute("viewBox", "0 0 24 24");
+checkmarkSvg.setAttribute("fill-opacity", "0");
 
 const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
 path.setAttribute(
   "d",
   "M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z",
 );
-path.setAttribute("fill", "#00e0bf");
 
 checkmarkSvg.appendChild(path);
 
@@ -69,17 +69,81 @@ function generateRandomArray(length) {
   }
   return randomArray;
 }
-
-const textContainer = document.querySelector(".text-box");
 const randomArray = generateRandomArray(12);
+const textInput = document.querySelector(".text-input");
+const textContainer = document.querySelector(".text-box");
 const textProgress = document.querySelector(".progress-container");
+const progressBar = document.querySelector(".progress-bar");
+
+let fail = false;
+let timer = setInterval(() => {
+  const computedStyle = getComputedStyle(progressBar);
+  const width = parseFloat(computedStyle.getPropertyValue("--width")) || 100;
+
+  if (width <= 0.5) {
+    // alert("You lost");
+    // window.location.reload();
+  } else if (!fail) {
+    progressBar.style.setProperty("--width", width - 0.25);
+  }
+}, 15.625);
+
+textInput.addEventListener("blur", () => {
+  setTimeout(() => {
+    textInput.focus();
+  }, 0);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const circles = document.querySelectorAll(".progress-circle");
+  const bars = document.querySelectorAll(".bar");
+
+  textInput.addEventListener("input", () => {
+    let current = textInput.value[textInput.value.length - 1];
+    if (current.toUpperCase() === randomArray[textInput.value.length - 1]) {
+      textContainer.children[textInput.value.length - 1].classList.add(
+        "correct",
+      );
+      circles[textInput.value.length - 1].classList.add(
+        "progress-circle-correct",
+      );
+      circles[textInput.value.length - 1].children[0].setAttribute(
+        "fill-opacity",
+        "1",
+        "fill",
+        "#00e0bf",
+      );
+      circles[textInput.value.length - 1].children[0].setAttribute(
+        "fill",
+        "#00e0bf",
+      );
+      bars[textInput.value.length - 1].classList.add("bar-correct");
+    } else {
+      textContainer.children[textInput.value.length - 1].classList.add(
+        "incorrect",
+      );
+      bars[textInput.value.length - 1].classList.add("bar-incorrect");
+      circles[textInput.value.length - 1].classList.add(
+        "progress-circle-incorrect",
+      );
+      circles[textInput.value.length - 1].children[0].setAttribute(
+        "fill-opacity",
+        "1",
+      );
+      circles[textInput.value.length - 1].children[0].setAttribute(
+        "fill",
+        "#ff3d00",
+      );
+    }
+  });
+});
 
 randomArray.map((item, index) => {
   const span = document.createElement("span");
   const progress = document.createElement("div");
   const bar = document.createElement("div");
   span.classList.add("letter");
-  progress.classList.add("progress-circle-correct");
+  progress.classList.add("progress-circle");
   progress.appendChild(checkmarkSvg.cloneNode(true));
   bar.classList.add("bar");
   span.textContent = item;
@@ -89,5 +153,3 @@ randomArray.map((item, index) => {
     textProgress.appendChild(bar);
   }
 });
-
-console.log(randomArray.join(""));
